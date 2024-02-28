165037cad1c647006d12f74557d58c2bb12e6d38
Altana <altanagrishkina@gmail.com>
2024-02-28 19:11:57 +0100

    Subscribing to the Store to Get State Changes

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -3,7 +3,6 @@ import { createAction, createReducer, on } from "@ngrx/store";
 export const productReducer = createReducer(
   { showProductCode: true },
   on(createAction('[Product] Toogle Product Code'), state => {
-    console.log('original state: ' + JSON.stringify(state));
         return {
           ...state,
           showProductCode: !state.showProductCode
