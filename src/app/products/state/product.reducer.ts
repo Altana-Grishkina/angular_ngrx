f254a8769f4bcbb980a6a18a58f6d1e214f9a5a0
Altana <altanagrishkina@gmail.com>
2024-03-06 18:35:41 +0100

    Extending the State Interface for Lazy Loaded Features

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,4 +1,16 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import * as AppState from '../../state/app.state';
+import { Product } from "../product";
+
+export interface State extends AppState.State {
+  products: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: ProductState;
+}
 
 export const productReducer = createReducer(
   { showProductCode: true },
