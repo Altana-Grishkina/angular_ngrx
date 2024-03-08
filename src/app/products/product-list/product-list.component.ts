a9677b566edae69be3a900ec3f98eea210510cb1
Altana <altanagrishkina@gmail.com>
2024-03-08 18:00:59 +0100

    Demo: Using Strongly Typed Actions

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,6 +6,7 @@ import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { Store } from '@ngrx/store';
 import { getShowProductCode } from '../state/product.reducer';
+import * as ProductActions from '../state/product.action';
 
 @Component({
   selector: 'pm-product-list',
@@ -48,9 +49,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
   }
 
   checkChanged(): void {
-    this.store.dispatch(
-      { type: '[Product] Toogle Product Code'}
-    );
+    this.store.dispatch(ProductActions.toggleProductCode());
   }
 
   newProduct(): void {
--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,5 +1,6 @@
 import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
 import * as AppState from '../../state/app.state';
+import * as ProductActions from './product.action';
 import { Product } from "../product";
 
 export interface State extends AppState.State {
@@ -34,10 +35,34 @@ export const getProducts = createSelector(
 
 export const productReducer = createReducer<ProductState>(
   initialState,
-  on(createAction('[Product] Toogle Product Code'), (state): ProductState => {
+  on(ProductActions.toggleProductCode, (state): ProductState => {
         return {
           ...state,
           showProductCode: !state.showProductCode
       };
-  })
+  }),
+  on(ProductActions.setCurrentProduct, (state, action): ProductState => {
+    return {
+      ...state,
+      currentProduct: action.product
+    };
+  }),
+  on(ProductActions.clearCurrentProduct, (state, action): ProductState => {
+    return {
+      ...state,
+      currentProduct: null
+    };
+  }),
+  on(ProductActions.initializeCurrentProduct, (state, action): ProductState => {
+    return {
+      ...state,
+      currentProduct: {
+        id: 0,
+        productName: '',
+        productCode: 'New',
+        description: '',
+        starRating: 0
+      }
+    };
+  }),
 );
