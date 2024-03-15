a3aff76d12832b9a970fcdfcd027df83af197cfc
Altana <altanagrishkina@gmail.com>
2024-03-15 11:30:19 +0100

    Strongly Typing the Actions with Action Creators

--- src/app/products/state/product.action.ts
+++ src/app/products/state/product.action.ts
@@ -23,3 +23,10 @@ export const loadProducts = createAction('[Product] Load');
 export const loadProductsSuccess = createAction('[Product] Load Success', props<{ products: Product[] }>());
 
 export const loadProductsFailure = createAction('[Product] Load Fail', props<{ error: string }>());
+
+//when user click save
+export const updateProduct = createAction('[Product] Update Product', props<{ product: Product}>());
+
+export const updateProductSuccess = createAction('[Product] Update Product Success', props<{ product: Product}>());
+
+export const updateProductFailure = createAction('[Product] Update Product Fail', props<{ error: string}>());
