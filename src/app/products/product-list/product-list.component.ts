8b44ce62e07edb71f9ac2499fb9d776f0ca226c0
Altana <altanagrishkina@gmail.com>
2024-03-23 17:46:24 +0100

    Container component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,51 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Observable, Subscription } from 'rxjs';
-
-import { Product } from '../product';
-import { Store } from '@ngrx/store';
-import { getShowProductCode, State, getCurrentProduct, getProducts, getError } from '../state/product.reducer';
-import * as ProductActions from '../state/product.action';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent {
   pageTitle = 'Products';
 
-  products$: Observable<Product[]>;
-  selectedProduct$: Observable<Product>;
-  displayCode$: Observable<boolean>;
-  errorMessage$: Observable<string>;
-
-  constructor(private store: Store<any>) { }
-
-  ngOnInit(): void {
-
-    this.products$ = this.store.select(getProducts);
-
-    this.errorMessage$ = this.store.select(getError);
-
-    this.store.dispatch(ProductActions.loadProducts());
-    this.selectedProduct$ = this.store.select(getCurrentProduct);
-
-    this.displayCode$ = this.store.select(getShowProductCode);
-  }
-
-
-
-  checkChanged(): void {
-    this.store.dispatch(ProductActions.toggleProductCode());
-  }
-
-  newProduct(): void {
-    this.store.dispatch(ProductActions.initializeCurrentProduct());
-  }
-
-  productSelected(product: Product): void {
-    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId: product.id }));
-  }
+  
 
 }
